8c2207a1878a208a3a17ae0b4f91ac1cfef446d9
Arwent <mrxjkz02@gmail.com>
2022-07-18 14:32:47 +0200

    Configurable key to toggle auto pickup & optimization to avoid FPS loss
    and versioning, close #3 and close #4

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections;
+using System.Linq;
 using BepInEx;
 using BepInEx.Configuration;
 using Mirror;
@@ -10,9 +11,14 @@ namespace AutoPickup;
 public class Plugin : BaseUnityPlugin
 {
     private readonly ConfigEntry<float> _distance;
+    private readonly ConfigEntry<KeyCode> _toggle;
+    private bool _isEnabled;
 
     public Plugin()
     {
+        _toggle = Config.Bind("General", "Toggle", KeyCode.F1,
+            "The Unity's KeyCode that will be used to toggle auto pickup in game.");
+
         _distance = Config.Bind("General", "Distance", 2.0f,
             "The distance the player will be able to auto pickup drops.");
     }
@@ -22,20 +28,41 @@ public class Plugin : BaseUnityPlugin
         Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
     }
 
+    private void Start()
+    {
+        StartCoroutine(DoPickup());
+    }
+
     private void Update()
     {
-        if (FindObjectsOfType<DroppedItem>().Length <= 0 && Inventory.inv.localChar == null)
-            return;
+        if (!Input.GetKeyDown(_toggle.Value)) return;
 
-        foreach (var pickup in FindObjectsOfType<DroppedItem>().ToList().Where(pickup =>
-                     WorldManager.manageWorld.itemsOnGround.Contains(pickup) &&
-                     Vector3.Distance(Inventory.inv.localChar.GetComponent<CharMovement>().transform.position,
-                         pickup.transform.position) <= _distance.Value))
-        {
-            if (!Inventory.inv.addItemToInventory(pickup.myItemId, pickup.stackAmount)) continue;
+        _isEnabled = !_isEnabled;
+        NotificationManager.manage.createChatNotification(
+            $"AutoPick is now {(_isEnabled ? "activated" : "deactivated")}.");
+    }
 
-            WorldManager.manageWorld.itemsOnGround.Remove(pickup);
-            NetworkServer.Destroy(pickup.gameObject);
-        }
+    private IEnumerator DoPickup()
+    {
+        while (true)
+            if (!_isEnabled || (FindObjectsOfType<DroppedItem>().Length <= 0 && Inventory.inv.localChar == null))
+            {
+                yield return 0;
+            }
+            else
+            {
+                foreach (var pickup in FindObjectsOfType<DroppedItem>().ToList().Where(pickup =>
+                             WorldManager.manageWorld.itemsOnGround.Contains(pickup) &&
+                             Vector3.Distance(Inventory.inv.localChar.GetComponent<CharMovement>().transform.position,
+                                 pickup.transform.position) <= _distance.Value))
+                {
+                    if (!Inventory.inv.addItemToInventory(pickup.myItemId, pickup.stackAmount)) continue;
+                    SoundManager.manage.play2DSound(SoundManager.manage.pickUpItem);
+                    WorldManager.manageWorld.itemsOnGround.Remove(pickup);
+                    NetworkServer.Destroy(pickup.gameObject);
+                }
+
+                yield return new WaitForSeconds(0.4f);
+            }
     }
 }
